4e847cb05cbd3762bf7518490e378a43d1b59a99
Aleksey_T <talekseys@mail.ru>
2023-05-10 00:15:37 +0300

    Sdelal Again

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-    int result = cost / 20;
-    return result;
+        int result = cost / 20;
+        return result;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 11000;
+        int cost = 10000;
         int miles = service.calculate(cost);
 
 
